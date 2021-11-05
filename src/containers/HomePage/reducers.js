09f8b420d65bce33e728aa4532ad129019eb07ab
annpermi <perminova.anna85@gmail.com>
2021-11-05 16:36:48 -0400

    Setup redux

---
+++ src/containers/HomePage/reducers.js
@@ -0,0 +1,9 @@
+const defaultState = {};
+
+export default function homePageReducer(state = defaultState, action) {
+  //check action
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
